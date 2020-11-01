04dfd7691b98e93a7e04f2830a384ee7ba22663f
widepixel <61685625+widepixel@users.noreply.github.com>
2020-11-01 13:46:50 +0200

    Create read_byte.php

---
+++ read_byte.php
@@ -0,0 +1,17 @@
+<?php
+	
+	function getFileSize($fs){
+		
+		$ui = Array("b", "kb", "mb", "gb", "tb", "pb", "eb", "zb", "yb");
+		
+		for($i=0; $i < count($ui); $i++){
+			if($fs >= pow(1024, $i)){
+				$result = round($fs / pow(1024, $i), 2) ." ".$ui[$i];
+			}
+		}
+
+		return $result;
+
+	}
+
+?>
