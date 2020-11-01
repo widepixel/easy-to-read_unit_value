06ab0ce573670a4a89f4e108bad91a48035f4b47
widepixel <61685625+widepixel@users.noreply.github.com>
2020-11-01 13:57:05 +0200

    Update read_byte.php

--- read_byte.php
+++ read_byte.php
@@ -1,12 +1,12 @@
 <?php
 	
-	function getFileSize($fs){
+	function getFileSize($bytes){
 		
 		$ui = Array("b", "kb", "mb", "gb", "tb", "pb", "eb", "zb", "yb");
 		
 		for($i=0; $i < count($ui); $i++){
-			if($fs >= pow(1024, $i)){
-				$result = round($fs / pow(1024, $i), 2) ." ".$ui[$i];
+			if($bytes >= pow(1024, $i)){
+				$result = round($bytes / pow(1024, $i), 2) ." ".$ui[$i];
 			}
 		}
 
